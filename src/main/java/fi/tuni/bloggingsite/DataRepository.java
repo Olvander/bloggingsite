f65397861d3435204d93f33e04d7ef9b19948539
Olli Pertovaara <olli.pertovaara@tuni.fi>
2019-05-02 17:47:24 +0300

    Edit interface DataRepository.java
    - Make the interface inherit CrudRepository
    - Add one extra method to the repository
    - Add comments for javadoc

--- src/main/java/fi/tuni/bloggingsite/DataRepository.java
+++ src/main/java/fi/tuni/bloggingsite/DataRepository.java
@@ -1,4 +1,25 @@
 package fi.tuni.bloggingsite;
 
-public interface DataRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * This inteface manages Data type objects and inherits the CrudRepository
+ *
+ * Holds one custom extra method in addition to the existing methods.
+ *
+ * @author  Olli Pertovaara
+ * @version 2019.05.02
+ * @since   1.8
+ */
+public interface DataRepository extends CrudRepository<Data, Integer> {
+
+    /**
+     * Finds a Data object comparing its url ending with a title parameter.
+     *
+     * @param title The title of a blog post.
+     * @return      An optional Data object.
+     */
+    Optional<Data> findDataByUrlEndingWith(String title);
 }
