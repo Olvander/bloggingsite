979884924b5b91f35989bc499a91477ca37aea77
Olli Pertovaara <olli.pertovaara@tuni.fi>
2019-05-02 18:48:23 +0300

    Edit file index.js
    - Add code for rendering either submitted post or homepage

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
+import Homepage from './components/Homepage.js';
+import Data from './components/Data.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (document.referrer.match('http://localhost:8080/blog/get/.+')) {
+    let backendUrl = "http://localhost:8080/blogs/" + document.referrer.substring(31);
+    ReactDOM.render(<App component={<Data backendUrl={backendUrl}/>} />,
+        document.getElementById("root"));
+} else {
+    ReactDOM.render(<App component={<Homepage/>}/>, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
