fb9b7647f28d55462829f33b028d543f3a481949
Olli Pertovaara <olli.pertovaara@tuni.fi>
2019-05-02 18:13:18 +0300

    Edit class MyConfiguration.java
    - Allow CORS mappings for all origins
    - Add comments for javadoc

--- src/main/java/fi/tuni/bloggingsite/MyConfiguration.java
+++ src/main/java/fi/tuni/bloggingsite/MyConfiguration.java
@@ -1,4 +1,26 @@
 package fi.tuni.bloggingsite;
 
-public class MyConfiguration {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class used for allowing CORS requests.
+ *
+ * @author  Olli Pertovaara
+ * @version 2019.05.02
+ * @since   1.8
+ */
+public class MyConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Allows cors mappings for GET, POST, PUT and DELETE from all origins.
+     *
+     * @param registry  The CorsRegistry object used with adding mappings.
+     */
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+    }
 }
