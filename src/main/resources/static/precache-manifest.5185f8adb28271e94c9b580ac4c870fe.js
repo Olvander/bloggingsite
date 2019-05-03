0d314f60d64c0bebfab8af3c83c05d9b3c6b74af
Olli Pertovaara <olli.pertovaara@tuni.fi>
2019-05-03 12:46:20 +0300

    Add static files for running frontend

---
+++ src/main/resources/static/precache-manifest.5185f8adb28271e94c9b580ac4c870fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61670511212828173a8194534a64abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46c0f7755972f9ea2cd",
+    "url": "/static/css/main.e6c8f4a4.chunk.css"
+  },
+  {
+    "revision": "5c5ba1a6feb28b7c3f94",
+    "url": "/static/js/2.6fcf8785.chunk.js"
+  },
+  {
+    "revision": "f46c0f7755972f9ea2cd",
+    "url": "/static/js/main.e325143d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
